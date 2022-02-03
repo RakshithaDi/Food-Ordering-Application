3323a1b8563e5fa03dd2c1479cf96b6c884ce8b9
Rakshitha <rakshithadilshan1@gmail.com>
2022-02-03 13:00:48 +0530

    login ui updated

--- food_ordering_application/lib/Authentication/login.dart
+++ food_ordering_application/lib/Authentication/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class login extends StatefulWidget {
   //const login({Key? key}) : super(key: key);
@@ -15,99 +16,164 @@ class _loginState extends State<login> {
       home: Scaffold(
         backgroundColor: Colors.grey[100],
         body: SafeArea(
-          child: Column(
+          child: ListView(
             children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Center(
-                  child: Container(
-                    margin: EdgeInsets.only(
-                      top: 5,
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image(
-                        image: AssetImage('images/foodx.png'),
-                        height: 100.0,
-                        width: 200.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Text(
-                'Welcome to FoodX!',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text(
-                'Lets help you meet up your tasks.',
-                style: TextStyle(fontSize: 15, color: Colors.grey),
-              ),
-              SizedBox(
-                height: 10.0,
-                width: 350,
-                child: Divider(
-                  thickness: 2,
-                  color: Colors.grey[400],
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Expanded(
-                flex: 2,
+              Center(
                 child: Container(
-                  margin: EdgeInsets.only(right: 30, left: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.email),
-                          labelText: 'Email',
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      TextField(
-                        style: TextStyle(height: 1),
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          prefixIcon: Icon(Icons.remove_red_eye),
-                          labelText: 'Password',
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      SizedBox(
-                        width: 250,
-                        height: 50,
-                        child: TextButton(
-                          style: TextButton.styleFrom(
-                            primary: Colors.white,
-                            backgroundColor: Color(0XFFD8352C),
-                          ),
-                          onPressed: () {},
-                          child: Text('Sign In'),
-                        ),
-                      ),
-                    ],
+                  margin: EdgeInsets.only(
+                    top: 30,
+                    bottom: 30,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image(
+                      image: AssetImage('images/foodx.png'),
+                      height: 100.0,
+                      width: 200.0,
+                    ),
                   ),
                 ),
               ),
               Container(
+                margin: EdgeInsets.only(bottom: 10),
                 child: Column(
-                  children: [
-                    Text('ffff'),
+                  children: <Widget>[
+                    Text(
+                      'Welcome to FoodX!',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      'Lets help you meet up your tasks.',
+                      style: TextStyle(fontSize: 15, color: Colors.grey),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                      width: 350,
+                      child: Divider(
+                        thickness: 2,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
                   ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(right: 30, left: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.email),
+                        labelText: 'Email',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    TextField(
+                      style: TextStyle(height: 1),
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.remove_red_eye),
+                        labelText: 'Password',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                margin: EdgeInsets.only(right: 30),
+                height: 40,
+                child: SizedBox(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      primary: Colors.black,
+                      //backgroundColor: Color(0XFFD8352C),
+                      textStyle: TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text('Forgot Password?'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(right: 30, left: 30),
+                child: SizedBox(
+                  width: 250,
+                  height: 50,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      backgroundColor: Color(0XFFD8352C),
+                    ),
+                    onPressed: () {},
+                    child: Text('Sign In'),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                margin: EdgeInsets.only(right: 30, left: 30),
+                child: SizedBox(
+                  height: 50,
+                  child: TextButton.icon(
+                    style: TextButton.styleFrom(
+                        //primary: Colors.white,
+                        // backgroundColor: Color(0XFFD8352C),
+
+                        side: BorderSide(color: Colors.black87, width: 1)),
+                    onPressed: () {},
+                    icon: FaIcon(FontAwesomeIcons.google),
+                    label: Text(
+                      'Sign In With Google',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                margin: EdgeInsets.only(right: 30, left: 30),
+                child: SizedBox(
+                  height: 50,
+                  child: TextButton.icon(
+                    style: TextButton.styleFrom(
+                        //primary: Colors.white,
+                        // backgroundColor: Color(0XFFD8352C),
+
+                        side: BorderSide(color: Colors.black87, width: 1)),
+                    onPressed: () {},
+                    icon: FaIcon(FontAwesomeIcons.google),
+                    label: Text(
+                      'Sign In With Google',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
