af2cb57e6484c4d1b4f7f333f0d070b28d69d18d
Rakshitha <rakshithadilshan1@gmail.com>
2022-02-17 22:47:49 +0530

    added advertisment list

--- food_ordering_application/lib/Home/menu.dart
+++ food_ordering_application/lib/Home/menu.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:anim_search_bar/anim_search_bar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../constant.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 
 class Menu extends StatefulWidget {
   @override
@@ -9,6 +10,21 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+  var currentPageValue = 0.0;
+  double scaleFactor = 0.8;
+  double height = 140.0;
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        currentPageValue = pageController.page;
+        print(currentPageValue.toString());
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +34,7 @@ class _MenuState extends State<Menu> {
           child: ListView(
             children: [
               Container(
-                height: 190,
+                height: 130,
                 color: kredbackgroundcolor,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -79,30 +95,87 @@ class _MenuState extends State<Menu> {
                   ],
                 ),
               ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                height: 200,
-                child: PageView.builder(
-                    itemCount: 3,
-                    itemBuilder: (context, position) {
-                      return Container(
-                        margin: EdgeInsets.only(left: 5, right: 5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.grey,
-                          image: DecorationImage(
-                            image: AssetImage('images/ad.jpg'),
-                          ),
-                        ),
-                        child: Column(
-                          children: [],
-                        ),
-                      );
-                    }),
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 5),
+                    height: 140,
+                    child: PageView.builder(
+                        controller: pageController,
+                        itemCount: 3,
+                        itemBuilder: (context, position) {
+                          return buildPageItem(position);
+                        }),
+                  ),
+                  new DotsIndicator(
+                    dotsCount: 3,
+                    position: currentPageValue,
+                  ),
+                ],
               ),
+              // Container(
+              //   child: ListView(
+              //     scrollDirection: Axis.horizontal,
+              //     children: [
+              //       Container(
+              //         height: 60,
+              //         width: 60,
+              //         color: Colors.grey,
+              //       ),
+              //     ],
+              //   ),
+              // ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  //advertisment sliding animation
+  Widget buildPageItem(int index) {
+    Matrix4 matrix = new Matrix4.identity();
+    if (index == currentPageValue.floor()) {
+      var currScale = 1 - (currentPageValue - index) * (1 - scaleFactor);
+      var currtrans = height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currtrans, 0);
+    } else if (index == currentPageValue.floor() + 1) {
+      var currScale =
+          scaleFactor + (currentPageValue - index + 1) * (1 - scaleFactor);
+      var currtrans = height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currtrans, 0);
+    } else if (index == currentPageValue.floor() - 1) {
+      var currScale = 1 - (currentPageValue - index) * (1 - scaleFactor);
+      var currtrans = height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currtrans, 0);
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, height * (1 - scaleFactor) / 2, 1);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 130,
+            margin: EdgeInsets.only(left: 5, right: 5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.grey,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('images/ad.jpg'),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
