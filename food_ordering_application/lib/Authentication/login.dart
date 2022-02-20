fd40e7c3eeb77c65b8c89ea71ff90e6a94638626
Rakshitha <rakshithadilshan1@gmail.com>
2022-02-20 15:41:19 +0530

    f

--- food_ordering_application/lib/Authentication/login.dart
+++ food_ordering_application/lib/Authentication/login.dart
@@ -43,24 +43,6 @@ class _LoginState extends State<Login> {
     }
   }
 
-  //Login function
-  // static Future<User> _signInWithEmailAndPassword(
-  //     {String email, String password, BuildContext context}) async {
-  //   FirebaseAuth auth = FirebaseAuth.instance;
-  //   User user;
-  //   try {
-  //     UserCredential userCredential = await FirebaseAuth.instance
-  //         .signInWithEmailAndPassword(email: email, password: password);
-  //   } on FirebaseAuthException catch (e) {
-  //     if (e.code == 'user-not-found') {
-  //       print('No user found for that email.');
-  //     } else if (e.code == 'wrong-password') {
-  //       print('Wrong password provided for that user.');
-  //     }
-  //   }
-  //   return user;
-  // }
-
   final _formKey = GlobalKey<FormState>();
   bool value = false;
   final maxLines = 5;
